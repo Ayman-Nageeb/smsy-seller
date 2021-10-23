d3e4a2f1b11cce881ba3b71e2af7c00df0647fe7
Ayman Nageeb <aimangta123121@gmail.com>
2021-10-23 11:02:38 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
